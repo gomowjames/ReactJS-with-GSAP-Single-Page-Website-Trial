3a9ac5d38e7c25a5fe395ff1bd09be192cad75b6
Barrett J Butterfield <gomowjames@users.noreply.github.com>
2022-02-03 18:32:07 -0800

    Rework of preloader functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { GlobalStyle } from './utils/cssGlobal';
 
+import LoadingAnim from './components/PreLoader';
 import Header from './components/Header';
 import Intro from './components/Intro';
 import ConsumerJour1 from './components/ConsumerJour1';
@@ -10,55 +11,65 @@ import ConsumerJour3 from './components/ConsumerJour3';
 import ValueProps from './components/ValueProps';
 import Testimonial from './components/Testimonial';
 import Footer from './components/Footer';
+import * as images from './assets/img';
+
+const imagesList = Object.values(images);
+
+function App() {
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [ count, setCounter ] = useState(0);
+
+  useEffect(() => {
+    if (count >= imagesList.length) {
+      setIsLoading(false);
+    }
+  }, [count])
+
+  const loadImages = () => {
+    return imagesList.map( src => 
+      <img
+        key={src}
+        src={src}
+        alt="img preload"
+        onLoad={() => setCounter( count + 1 )}
+      />
+    );
+  };
+
+    return (
+      <main>
+        <GlobalStyle />
+
+        { isLoading ?
+          <LoadingAnim /> :
+          <>
+          <Header />
+          <Intro />
+          <BoxedContent>
+            <ConsumerJour1/>
+            <ConsumerJour2 />
+            <ConsumerJour3 />
+            <ValueProps />
+            <Testimonial />
+          </BoxedContent>
+          <Footer />          
+          </>
+        }
+
+        <ImgLoader>{loadImages()}</ImgLoader>
+      </main>
+    )
+}
 
 const BoxedContent = styled.div`
   margin : 0 auto;
 `;
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      isLoading: true,
-    }
-  }
-
-  removeLoader() {
-    const preL = document.querySelector("#preloader");
-    if (preL) {
-      preL.remove();
-      this.setState({
-        isLoading: false,
-      });
-    }
-  }
-
-  componentDidMount(){
-    setTimeout( () => this.removeLoader(), 2000)    
-  }
-  
-  render() {
-    if (this.state.isLoading) {
-      return null;
-    }
-
-    return (
-      <main>
-        <GlobalStyle />
-        <Header />
-        <Intro/>
-        <BoxedContent>
-          <ConsumerJour1/>
-          <ConsumerJour2 />
-          <ConsumerJour3 />
-          <ValueProps />
-          <Testimonial />
-        </BoxedContent>
-        <Footer />
-      </main>
-    ) 
-  }
-}
+const ImgLoader = styled.div`
+  display: none;
+  height: 0;
+  width: 0;
+  opacity: 0;
+`;
 
 export default App;
